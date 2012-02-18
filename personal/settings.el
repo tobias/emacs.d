b7f9e68d3f90f2a66cb3c86b72ad552c138f17d5
Tobias Crawley <tcrawley@gmail.com>
2012-02-18 14:03:46 -0500

    zap-upto-char

--- personal/settings.el
+++ personal/settings.el
@@ -81,6 +81,13 @@
 (setq deft-directory "~/Dropbox/notes/")
 (setq deft-text-mode 'markdown-mode)
 
+(defadvice zap-to-char (after my-zap-to-char-advice (arg char) activate)
+    "Kill up to the ARG'th occurence of CHAR, and leave CHAR. If
+  you are deleting forward, the CHAR is replaced and the point is
+  put before CHAR"
+    (insert char)
+    (if (< 0 arg) (forward-char -1)))
+
 (server-start)
 
 
