e9e11fe8b346215962931084749cd3ec0d801b5b
Tobias Crawley <tcrawley@gmail.com>
2012-02-21 09:36:37 -0500

    It helps if you actually save customizations.

--- personal/custom.el
+++ personal/custom.el
@@ -23,4 +23,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "#aaa")))))
+ '(cursor ((t (:background "#aaa"))))
+ '(hl-line ((t (:inherit highlight :background "#222" :foreground "#fff")))))
