c8215852e921c827f663b3a454dcb9df1d8e24e1
Toby Crawley <toby@tcrawley.org>
2013-01-19 11:14:29 -0500

    Add action to view html emails in browser.

--- modes/mail.el
+++ modes/mail.el
@@ -120,5 +120,23 @@
                    (set (car var) (cadr var)))
                account-vars)))))
 
+;; from http://www.emacswiki.org/emacs/mu4e
+(defun tc/mu4e-msgv-action-view-in-browser (msg)
+  "View the body of the message in a web browser."
+  (interactive)
+  (let ((html (mu4e-msg-field (mu4e-message-at-point t) :body-html))
+        (tmpfile (format "%s/%d.html" temporary-file-directory (random))))
+    (unless html (error "No html part for this message"))
+    (with-temp-file tmpfile
+      (insert
+       "<html>"
+       "<head><meta http-equiv=\"content-type\""
+       "content=\"text/html;charset=UTF-8\">"
+       html))
+    (browse-url (concat "file://" tmpfile))))
+
+(add-to-list 'mu4e-view-actions
+             '("View in browser" . tc/mu4e-msgv-action-view-in-browser) t)
+
 (global-set-key (kbd "C-x m") 'mu4e)
 
