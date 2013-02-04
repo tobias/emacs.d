282cf479722062999a06ad4ac273d4c99c9d83c0
Toby Crawley <toby@tcrawley.org>
2013-02-04 17:03:20 -0500

    Let mu4e update mail periodically.

--- modes/mail.el
+++ modes/mail.el
@@ -14,6 +14,7 @@
  mu4e-user-mail-address-list '("toby@tcrawley.org" "tcrawley@redhat.com")
  mu4e-html2text-command      "w3m -dump -cols 80 -T text/html"
  mu4e-get-mail-command       "offlineimap"
+ mu4e-update-interval        600
  tc/default-trash-folder     "/tcrawley.org/INBOX.Trash"
  tc/default-refile-folder    "/tcrawley.org/INBOX.Archive"
  mu4e-sent-folder            "/tcrawley.org/INBOX.Sent" ;; always overridden, but mu4e needs a default
