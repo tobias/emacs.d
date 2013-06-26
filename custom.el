6f9ecc3a74ffef3a20e274975e879a36ca1e98ed
Toby Crawley <toby@tcrawley.org>
2013-06-26 19:10:22 -0400

    Make ido subdirs a bit more visible.

--- custom.el
+++ custom.el
@@ -13,4 +13,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(erc-button ((t nil))))
+ '(erc-button ((t nil)))
+ '(ido-subdir ((t (:foreground "lawn green")))))
