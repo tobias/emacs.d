b11ce0b05700c02a32ea5956a0d3bbaafe4e21a4
Toby Crawley <toby@tcrawley.org>
2013-10-13 21:22:04 -0400

    Use ox-reveal.

--- modes/org.el
+++ modes/org.el
@@ -1,5 +1,9 @@
 (setq org-hide-leading-stars t)
 
+(require 'ox-reveal)
+;(setq org-reveal-root "file:///home/tcrawley/hack/reveal.js-2.5.0/")
+(setq org-reveal-root "./")
+
 ;; Make windmove work in org-mode:
 (add-hook 'org-shiftup-final-hook 'windmove-up)
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
