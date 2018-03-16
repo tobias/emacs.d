fcfe1fefbe7b793873465033ea88d24c4b87ec9b
tcrawley <toby@tcrawley.org>
2018-03-16 13:29:59 -0400

    Allow git summary to go to 70 chars before warning

--- custom.el
+++ custom.el
@@ -9,7 +9,11 @@
  '(custom-safe-themes
    (quote
     ("eb9c9351518312620b1868cbd53cb4f8f50663075d11bf6dd5e82cf6810e0beb" "0866b5a8cc761ac024b0f6fa22ce9c86d637ef33e883a6a03a1290b2859a2b32" "39142fc4b4061be31353a9ec4c894b1bb71063881b0f8ebaee28611004fcc3b4" default)))
- '(fci-rule-color "#383838"))
+ '(fci-rule-color "#383838")
+ '(git-commit-summary-max-length 70)
+ '(package-selected-packages
+   (quote
+    (plantuml-mode whitespace-cleanup-mode restclient ido-completing-read+ elpy git-gutter json-mode mustache-mode cider yaml-mode websocket toml-mode swift-mode smex scala-mode rust-mode request rainbow-delimiters powerline oauth2 maxframe markdown-mode magit lua-mode link js2-mode jinja2-mode jade-mode inf-ruby ido-vertical-mode ido-ubiquitous idle-highlight htmlize highlight-parentheses helm-swoop guru-mode groovy-mode gradle-mode go-mode git-link gist fuzzy fold-dwim-org flymake-cursor flycheck-swift fish-mode find-things-fast expand-region erc-hl-nicks emojify editorconfig downplay-mode dockerfile-mode dired-details diminish dictionary connection cm-mode clj-refactor circe browse-kill-ring auto-complete align-cljlet alert adoc-mode ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
