8c850cf288dc22797d2aa2a6699ad3774a9014b6
tcrawley <toby@tcrawley.org>
2018-03-16 13:40:02 -0400

    Allow plantuml usage in org

--- lisp/modes/org.el
+++ lisp/modes/org.el
@@ -4,9 +4,18 @@
 ;;(setq org-reveal-root "file:///home/tcrawley/hack/reveal.js-2.5.0/")
 ;;(setq org-reveal-root "./")
 
+(require 'ox-odt)
+
 ;; Make windmove work in org-mode:
 (add-hook 'org-shiftup-final-hook 'windmove-up)
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
- (add-hook 'before-save-hook 'time-stamp)
+(add-hook 'before-save-hook 'time-stamp)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((plantuml . t)))
+
+(setq org-plantuml-jar-path
+      "/usr/local/Cellar/plantuml/1.2018.2/libexec/plantuml.jar")
